e83b266266d6c10695a293d4e5081b125a14bce3
treesaretall <himatthewhurst@hotmail.com>
2023-09-17 14:57:46 -0400

    troubleshooting create user

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
